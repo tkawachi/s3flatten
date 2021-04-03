709bc6e59f8ff9bb2b52c9f46769db12516d1b48
Takashi Kawachi <tkawachi@gmail.com>
2021-04-03 22:06:56 +0900

    working

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tkawachi/s3flatten
 go 1.16
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.3.1
 	github.com/aws/aws-sdk-go-v2/config v1.1.4
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.4.0
 	github.com/pborman/getopt/v2 v2.1.0
--- s3flatten.go
+++ s3flatten.go
@@ -2,17 +2,142 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"net/url"
 	"os"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/pborman/getopt/v2"
 )
 
+var verbose bool
+
+type s3Path struct {
+	bucket string
+	prefix string
+}
+
+type copyResult struct {
+	srcKey string
+	err    error
+}
+
+func debug(v ...interface{}) {
+	if verbose {
+		log.Println(v...)
+	}
+}
+
+func parseS3Path(s string) (*s3Path, error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse as S3 path: %s", s)
+	}
+	if u.Scheme != "s3" {
+		return nil, fmt.Errorf("S3 path should start with `s3`: %s", s)
+	}
+	prefix := u.Path
+	if !strings.HasSuffix(prefix, "/") {
+		prefix += "/"
+	}
+	if strings.HasPrefix(prefix, "/") {
+		prefix = prefix[1:]
+	}
+	return &s3Path{
+		bucket: u.Host,
+		prefix: prefix,
+	}, nil
+}
+
+func genDstKey(srcKey, srcKeyPrefix, dstKeyPrefix, delimiter string) (string, error) {
+	if !strings.HasPrefix(srcKey, srcKeyPrefix) {
+		return "", fmt.Errorf("srcKeyPrefix not match. srcKey: %s, srcKeyPrefix: %s", srcKey, srcKeyPrefix)
+	}
+	return (dstKeyPrefix + strings.Replace(srcKey[len(srcKeyPrefix):], "/", delimiter, -1)), nil
+}
+
+func copyObject(inCh <-chan string, outCh chan<- copyResult, client *s3.Client, srcPath, dstPath *s3Path, delimiter string) {
+	for {
+		srcKey, ok := <-inCh
+		if !ok {
+			return
+		}
+		dstKey, err := genDstKey(srcKey, srcPath.prefix, dstPath.prefix, delimiter)
+		if err != nil {
+			outCh <- copyResult{srcKey, err}
+			continue
+		}
+		input := &s3.CopyObjectInput{
+			CopySource: aws.String(srcPath.bucket + "/" + srcKey),
+			Bucket:     aws.String(dstPath.bucket),
+			Key:        aws.String(dstKey),
+		}
+		debug("Starting copy:", srcKey)
+		startTime := time.Now()
+		_, err = client.CopyObject(context.Background(), input)
+		if err != nil {
+			outCh <- copyResult{srcKey, err}
+		}
+		debug("Finished copy:", srcKey, time.Now().Sub(startTime))
+		outCh <- copyResult{srcKey, nil}
+	}
+}
+
+func watchComplete(complete chan<- error, inCh <-chan string, outCh <-chan copyResult) {
+	defer close(complete)
+	completed := make(map[string]bool)
+	inChClosed := false
+	isFinished := func() bool {
+		if !inChClosed {
+			return false
+		}
+		hasIncompleted := false
+		for _, b := range completed {
+			if !b {
+				hasIncompleted = true
+				break
+			}
+		}
+		return !hasIncompleted
+	}
+	for {
+		select {
+		case srcKey, ok := <-inCh:
+			if ok {
+				completed[srcKey] = false
+			} else {
+				inChClosed = true
+				if isFinished() {
+					complete <- nil
+					return
+				}
+			}
+		case result := <-outCh:
+			if result.err != nil {
+				complete <- result.err
+				return
+			}
+			completed[result.srcKey] = true
+			if isFinished() {
+				complete <- nil
+				return
+			}
+		}
+	}
+}
+
 func main() {
 	helpFlag := getopt.BoolLong("help", 'h', "display help")
 	delimiterFlag := getopt.StringLong("delimiter", 'd', "-", "Delimiter to replace '/' with to flatten path.")
+	suffixFlag := getopt.StringLong("suffix", 's', "", "Copy only objects which has this suffix in key")
+	parallelFlag := getopt.IntLong("concurrency", 'c', 1000, "Number of coroutine for COPY operation")
+	getopt.FlagLong(&verbose, "verbose", 'v', "verbose output")
 	getopt.SetParameters("s3://src-bucket/path/to/src/ s3://dest-bucket/path/to/dest/")
 	getopt.Parse()
 	args := getopt.Args()
@@ -20,26 +145,66 @@ func main() {
 		getopt.Usage()
 		os.Exit(1)
 	}
-	log.Println(*delimiterFlag)
+	srcPath, err := parseS3Path(args[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	dstPath, err := parseS3Path(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Println(args)
+	debug("Delimiter:", *delimiterFlag)
+	debug("Source path:", srcPath)
+	debug("Destination path:", dstPath)
 
-	cfg, err := config.LoadDefaultConfig(context.TODO())
+	startTime := time.Now()
+	targetCount := 0
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
 	client := s3.NewFromConfig(cfg)
 
-	listInput := s3.ListObjectsV2Input{} // TODO specify bucket, prefix
+	watchInCh := make(chan string)
+	copyInCh := make(chan string)
+	outCh := make(chan copyResult)
+	completeCh := make(chan error)
+
+	go watchComplete(completeCh, watchInCh, outCh)
+	for i := 0; i < *parallelFlag; i++ {
+		go copyObject(copyInCh, outCh, client, srcPath, dstPath, *delimiterFlag)
+	}
+
+	listInput := s3.ListObjectsV2Input{
+		Bucket: aws.String(srcPath.bucket),
+		Prefix: aws.String(srcPath.prefix),
+	}
 	paginator := s3.NewListObjectsV2Paginator(client, &listInput)
 	for paginator.HasMorePages() {
-		page, err := paginator.NextPage(context.TODO())
+		page, err := paginator.NextPage(ctx)
 		if err != nil {
 			log.Printf("error: %v", err)
 			return
 		}
 		for _, value := range page.Contents {
-			log.Println(*value.Key)
+			if strings.HasSuffix(*value.Key, *suffixFlag) {
+				watchInCh <- *value.Key
+				copyInCh <- *value.Key
+				targetCount++
+			}
 		}
 	}
+	close(watchInCh)
+	close(copyInCh)
+	err = <-completeCh
+	if err != nil {
+		log.Fatal(err)
+	}
+	elapsed := time.Now().Sub(startTime)
+	// objects processed per 10 seconds
+	speed := float32(targetCount) / float32(elapsed/time.Second)
+	log.Printf("Copied %d items in %v, %.2f items/sec", targetCount, elapsed, speed)
 }
