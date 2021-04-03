cdbed5eb4dd5e03025fa7302d5ec62a58b505f02
Takashi Kawachi <tkawachi@gmail.com>
2021-04-03 14:36:59 +0900

    update

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tkawachi/s3flatten
 
 go 1.16
 
-require github.com/aws/aws-sdk-go v1.38.12
+require (
+	github.com/aws/aws-sdk-go v1.38.12
+	github.com/pborman/getopt/v2 v2.1.0
+)
--- s3flatten.go
+++ s3flatten.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/pborman/getopt/v2"
 )
 
 func main() {
+	helpFlag := getopt.BoolLong("help", 'h', "display help")
+	delimiterFlag := getopt.StringLong("delimiter", 'd', "-", "Delimiter to replace '/' with to flatten path.")
+	getopt.SetParameters("s3://src-bucket/path/to/src/ s3://dest-bucket/path/to/dest/")
+	getopt.Parse()
+	args := getopt.Args()
+	if *helpFlag || len(args) != 2 {
+		getopt.Usage()
+		os.Exit(1)
+	}
+	log.Println(*delimiterFlag)
+
+	log.Println(args)
+
 	sess := session.Must(session.NewSession())
 	svc := s3.New(sess)
 
